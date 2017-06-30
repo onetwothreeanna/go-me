cc8c85e23b3c6b3240a9a85a49ba8f25311ebacf
Anna Youngyeun <anna.youngyeun@gmail.com>
2017-06-30 17:47:16 -0500

    Initial commit.

---
+++ go-me/src/main/java/org/launchcode/gome/GoMeApplication.java
@@ -0,0 +1,12 @@
+package org.launchcode.gome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoMeApplication.class, args);
+	}
+}
---
+++ go-me/src/test/java/org/launchcode/gome/GoMeApplicationTests.java
@@ -0,0 +1,16 @@
+package org.launchcode.gome;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GoMeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
