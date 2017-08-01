de395c4115ef4f002b71786199c7602b45f63035
Anna Youngyeun <anna.youngyeun@gmail.com>
2017-07-31 20:05:40 -0500

    Made email form, updated to customizable send address.  TO DO: Fix error handling and model binding.

--- go-me/src/main/java/org/launchcode/gome/controllers/EmailController.java
+++ go-me/src/main/java/org/launchcode/gome/controllers/EmailController.java
@@ -1,17 +1,24 @@
 package org.launchcode.gome.controllers;
 
 import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 import org.launchcode.gome.models.Category;
+import org.launchcode.gome.models.EmailForm;
 import org.launchcode.gome.models.LogItem;
 import org.launchcode.gome.models.data.CategoryDao;
 import org.launchcode.gome.models.data.LogItemDao;
+import org.launchcode.gome.models.data.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +26,8 @@ import java.util.List;
 /*Starter code from: http://www.opencodez.com/java/java-mail-framework-using-spring-boot.htm */
 
 @Controller
+@RequestMapping("go-me")
+
 public class EmailController {
 
     @Autowired
@@ -27,24 +36,30 @@ public class EmailController {
     private LogItemDao logItemDao;
     @Autowired
     private CategoryDao categoryDao;
+    @Autowired
+    private UserDao userDao;
 
     //TO DO Set up email form and page with ability to change TO and FROM.
-    @RequestMapping("/email")
-    @ResponseBody
-    String home() {
-        try {
-            sendEmail();
-            return "Email Sent!";
-        }catch(Exception ex) {
-            return "Error in sending email: "+ex;
-        }
+    @RequestMapping(value = "email", method = RequestMethod.GET)
+    public String loadEmailForm(Model model) {
+        model.addAttribute("email", new EmailForm());
+        model.addAttribute("title", "go-me");
+        return "index/email";
     }
 
-    private void sendEmail() throws Exception{
+    @RequestMapping(value="email", method = RequestMethod.POST)
+    public String sendEmail(@ModelAttribute @Valid EmailForm email, Errors errors,
+                            Model model, HttpServletRequest request) throws Exception {
+
+        if(errors.hasErrors()){
+            model.addAttribute("title", "goMe");
+            return "index/email";
+        }
+
         MimeMessage message = sender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message);
         List<String> logItems = new ArrayList<>();
-        for(Category category: categoryDao.findAll()){
+        for(Category category: categoryDao.findByUserId(userDao.findByUsername(request.getSession().getAttribute("currentUser").toString()).getId())){
             for(LogItem logItem : category.getLogItems()) {
                 logItems.add(logItem.getDescription());
             }
@@ -55,10 +70,21 @@ public class EmailController {
                 .replace("[", "")  //remove the right bracket
                 .replace("]", "")  //remove the left bracket
                 .trim();           //remove trailing spaces from partially initialized arrays
-        helper.setTo("plentybyanna@gmail.com");
-        helper.setText("Heck yes.  You've accomplished " + logItems.size() + " tasks." + " \n\n Check out all you've gotten done: \n " + formattedString + "\n\n\nYou're so cool.");
-        helper.setSubject("Go you!");
+        helper.setTo(email.getTo());
+        helper.setFrom(email.getTo());
+        helper.setText(email.getFrom() + " wants to share a list of their accomplishments with you!" + "\n\n\n" + email.getMessage() + " \n\n " + logItems.size() + " tasks." + " \n\n " +
+                "Check out all you've gotten done: \n " + formattedString + "\n\n\nYou're so cool.");
+        helper.setSubject("Hooray productivity!");
 
         sender.send(message);
+
+        model.addAttribute("title", "goMe");
+        model.addAttribute("emailSent", "Email sent to " + email.getTo());
+
+        model.addAttribute(new LogItem());
+        model.addAttribute("logItems", logItemDao.findByUserId(userDao.findByUsername(request.getSession()
+                .getAttribute("currentUser").toString()).getId()));
+        return "index/email-sent";
     }
+
 }
